10415e47c5b3018189b872ae4b7ca9588782343e
rajashekhar korva <rajashekhar.korva@gmail.com>
2020-06-08 17:41:13 -0400

    Updated green cart

--- testWebA/src/test/java/testWebA/GreenCart.java
+++ testWebA/src/test/java/testWebA/GreenCart.java
@@ -1,6 +1,12 @@
 package testWebA;
 
+import org.testng.annotations.Test;
+
 public class GreenCart {
+	@Test
+	public void fruits() {
+		System.out.println("Fruits in the list");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub

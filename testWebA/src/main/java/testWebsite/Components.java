ab51d9f6f4e41984bdc455eeccefe75a380e8262
rajashekhar korva <rajashekhar.korva@gmail.com>
2020-06-08 17:07:57 -0400

    First Commit

---
+++ testWebA/src/main/java/testWebsite/Components.java
@@ -0,0 +1,10 @@
+package testWebsite;
+
+public class Components {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ testWebA/src/test/java/testWebA/GreenCart.java
@@ -0,0 +1,10 @@
+package testWebA;
+
+public class GreenCart {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
